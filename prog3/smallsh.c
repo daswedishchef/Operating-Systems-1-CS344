79d98e81124400aa176d3126c07c78ba42f5fd33
Spencer Carlson <carlsosp@os1.engr.oregonstate.edu>
2020-02-23 13:48:27 -0800

    Initial commit of assignment 3

---
+++ prog3/smallsh.c
@@ -0,0 +1,12 @@
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <fcntl.h>
+
+void main(){
+    printf("hello world\n");
+    return;
+}
