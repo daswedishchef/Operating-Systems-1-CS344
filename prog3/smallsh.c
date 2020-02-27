d217bcec30c6c546345ff491a8e6f34825042b9b
Spencer Carlson <carlsosp@os1.engr.oregonstate.edu>
2020-02-26 19:03:56 -0800

    more process management

--- prog3/smallsh.c
+++ prog3/smallsh.c
@@ -5,9 +5,10 @@
 #include <sys/types.h>
 #include <string.h>
 #include <fcntl.h>
-
+//global vars
 #define delim " \t\r\n\a"
 int globstat = 0;
+int engv = 0;
 
 void debug(char **input, int num){
     int i;
@@ -15,7 +16,7 @@ void debug(char **input, int num){
         printf("%s\n", input[i]);
     }
 }
-
+//Built-in commands
 int mystatus(char **input){
     printf("%s %d\n","exit value",globstat);
     return 0;
@@ -32,11 +33,12 @@ int myexit(char **input){
     exit(0);
     return 1;
 }
-
 int (*natives[])(char**) = {&mycd,&myexit,&mystatus};
 char *native[] = {"cd","exit","status"};
 
-int process(char **input){
+
+//Execution function
+int process(char **input, int bkfl){
     //similar to fork and execlp example in lecture
     int pstat = -5;
     pid_t mypid = -5;
@@ -44,42 +46,84 @@ int process(char **input){
     mypid = fork();
     switch(mypid){
         case -1: {
-            perror("Something went wrong\n");
+            //error case
             return 1;
             break;
         }
         case 0: {
+            //child goes here
+            globstat = 0;
             globstat = execvp(input[0], input);
-            perror("-There was a problem with that command-\n");
+            perror("");
             return 0;
-            break;
         }
         default: {
-            myotherpid = waitpid(mypid, &pstat, WUNTRACED);
-            return 0;
-            break;
+            //parent goes here
+            if((bkfl==0)&&(engv==0)){
+                //parent waits for foreground process
+                myotherpid = waitpid(mypid, &pstat, WUNTRACED);
+                return 0;
+            }
+            else if((bkfl==1)&&(engv==0)){
+                printf("process supposed to start in background\n");
+                myotherpid = waitpid(mypid, &pstat, WNOHANG);
+                return 0;
+            }
         }
     }
 }
-
+//This function stages the execution
 int mountain_doit(char **input, int num){
-    //array kept getting filled with trash after freeing not sure why
-    //used the index created by strtok and a loop in main
+    //check to see if user entered anything
     if(num == 0){
         printf("Please give an argument\n");
         return 0;
     }
+    //check if user entered built in command
     int i;
     for(i = 0; i < 3; i++){
         if(!strcmp(input[0], native[i])){
             return (*natives[i])(input);
         }
     }
-    return process(input);
+    //check for $$
+    for(i = 0;i < num; i++){
+        if(!strcmp(input[i], "$$")){
+            //input[i] = (char*)getpid();
+        }
+    }
+    if(!strcmp(input[0], "#")){
+        return 0;
+    }
+    //check for background specification
+    if(!strcmp(input[num-1], "&")){
+        input[num-1]=NULL;
+        return process(input,1);
+    }
+    else{
+        return process(input,0);
+    }
 }
-
+//signal handlers
+void inthand(int sig){
+    printf(" cntrl+c recieved\n");
+}
+void stphand(int sig){
+    if(engv==1){
+        char* msg1 = "\n-Exiting foreground only mode-\n";
+        write(STDOUT_FILENO, msg1, 36);
+        engv = 0;
+    }
+    else if(engv==0){
+        char* msg2 = "\n-Entering foreground only mode-\n";
+        write(STDOUT_FILENO, msg2, 37);
+        engv = 1;
+    }
+}
+//Main reads line, calls functions, and frees memory
 void main(){
     int stat = 0;
+    signal(SIGTSTP, stphand);
     do{
         char **output = malloc(sizeof(char*));
         char *input;
@@ -96,7 +140,7 @@ void main(){
             index += 1;
             arg = strtok(NULL, delim);
             //allocate more memory as the tokens are read
-            output = realloc(output, index *sizeof(char*));
+            output = realloc(output, (index+1) *sizeof(char*));
         }
         if(!output){
             printf("Something went wrong parsing commands\n");
@@ -110,7 +154,7 @@ void main(){
         free(input);
         free(output);
         int i;
-        for(i = 0;i<index;i++){
+        for(i = 0;i<index+1;i++){
             output[i]=NULL;
         }
    }while(stat == 0);
