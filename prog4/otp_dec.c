ea8aa74d40e3f381ec2cea4b1dd70b5c5c5b057a
Spencer Carlson <carlsosp@os1.engr.oregonstate.edu>
2020-03-14 04:31:49 -0700

    forgot to add dec files

---
+++ prog4/otp_dec.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <stdbool.h>
+
+void error(const char *msg) { perror(msg); } // Error function used for reporting issues
+
+bool wait_for_success(int socketFD) {
+	int charsRead;
+	char buffer[16];
+	printf("Client: Waiting for success\n");
+	memset(buffer, '\0', 16);
+	charsRead = recv(socketFD, buffer, 16, 0);
+	if (charsRead < 1 || strcmp(buffer, "success"))
+	{
+		fprintf(stderr, "Client: Server did not respond. It instead said: %s with %d\n", buffer, charsRead);
+		return false;
+	}
+	printf("Client: Got for success\n");
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	int socketFD, portNumber, charsWritten, charsRead;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+	char *buffer;
+    
+	if (argc < 4) { fprintf(stderr,"USAGE: %s plainttext key port\n", argv[0]); exit(0); } // Check usage & args
+
+	// Set up the server address struct
+	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+
+	// Set up the socket
+	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (socketFD < 0) error("CLIENT: ERROR opening socket");
+	
+	// Connect to server
+	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+		error("CLIENT: ERROR connecting");
+
+	// Get return message from server
+	buffer = malloc(16*sizeof(char));
+	memset(buffer, '\0', 4*sizeof(char)); // Clear out the buffer again for reuse
+	charsRead = recv(socketFD, buffer, 256, 0); // Read data from the socket, leaving \0 at end
+	if (charsRead < 3) error("CLIENT: ERROR reading from socket");
+	if(!strcmp(buffer,"dec")){
+		// Send message to server
+		int keynum,txnum;
+		char temp;
+		FILE *mytext;
+		FILE *mykey;
+		mytext = fopen(argv[1],"r");
+		if(mytext==NULL){
+			fprintf(stderr,"Unable to read plaintext\n");
+			return 1;
+		}
+		txnum = 0;
+		while(fgetc(mytext)!=EOF){
+			txnum++;
+		}
+		mykey = fopen(argv[2],"r");
+		if(mykey==NULL){
+			fprintf(stderr,"Unable to read plaintext\n");
+			return 1;
+		}
+		keynum = 0;
+		while(fgetc(mykey)!=EOF){
+			keynum++;
+		}
+		if(keynum>txnum){
+			fprintf(stderr, "key cannot be longer than text\n");
+			return 1;
+		}
+		char input[24];
+		sprintf(input,"%d",keynum);
+		//do{
+		charsWritten = send(socketFD, input, 24, 0);
+		//}while(charsWritten < keynum);
+		if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+		memset(buffer,'\0',16);
+		charsRead = recv(socketFD, buffer, 16, 0);
+		if(strcmp(buffer,"success")){
+			fprintf(stderr,"Client: Server did not respond\n");
+			return 1;
+		}
+		free(buffer);
+		buffer = calloc(keynum,(keynum+1)*sizeof(char));
+		rewind(mykey);
+		char *textbuff;
+		textbuff = malloc(keynum*sizeof(char));
+		fgets(textbuff,keynum,mykey);
+		//printf("sending: %s\n",textbuff);
+		charsWritten = send(socketFD, textbuff, keynum, 0);
+		//printf("Client: waiting for success");
+		memset(buffer,'\0',16);
+		charsRead = recv(socketFD, buffer, 16, 0);
+		if(strcmp(buffer,"success")){
+			fprintf(stderr,"Client: Server did not respond\n");
+			return 1;
+		}
+		//send text
+		memset(input, '\0', sizeof(input));
+		sprintf(input,"%d",txnum);
+		//printf("sending: %s\n",input);
+		charsWritten = send(socketFD, input, 24, 0);
+		if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
+		memset(buffer,'\0',16);
+		//printf("Client: waiting for success");
+		charsRead = recv(socketFD, buffer, 16, 0);
+		if(strcmp(buffer,"success")){
+			fprintf(stderr,"Client: Server did not respond\n");
+			return 1;
+		}
+		rewind(mytext);
+		char *pbuff;
+		pbuff = malloc(txnum*sizeof(char));
+		fgets(pbuff,txnum,mytext);
+		//printf("sending: %s\n",pbuff);
+		charsWritten = send(socketFD, pbuff, txnum, 0);
+		memset(pbuff,'\0',txnum);
+		charsRead = recv(socketFD, pbuff, txnum, 0);
+		if(charsRead < 0) error("CLIENT: ERROR retrieving cyphertext");
+		pbuff[txnum] = '\n';
+		fprintf(stdout,pbuff);
+		fclose(mytext);
+		fclose(mykey);
+		free(pbuff);
+		free(textbuff);
+	}
+	else{
+		fprintf(stderr,"This program connects exlusively with otp_enc_d\n");
+		return 1;
+	}
+	//printf("\nclient gang out\n");
+	close(socketFD); // Close the socket
+	return 0;
+}
---
+++ prog4/otp_dec_d.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <signal.h>
+//global vars
+char ops[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+int numchild = 0;
+
+void chldhand(int signal){
+	numchild--;
+}
+
+void send_success(int establishedConnectionFD) {
+	printf("SERVER: sending success\n");
+	send(establishedConnectionFD, "success", 16, 0);
+}
+
+char toOps(int num){
+	if(num>27){
+		fprintf(stderr,"error converting int");
+		return '0';
+	}
+	return ops[num];
+}
+
+int opsout(char op){
+	int i; 
+	i=0;
+	for(i=0;i<27;i++){
+		//printf("%d",i);
+		if(ops[i]==op){
+			return i;
+		}
+	}
+}
+
+void error(const char *msg) { perror(msg); } // Error function used for reporting issues
+
+int main(int argc, char *argv[])
+{
+	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
+	socklen_t sizeOfClientInfo;
+	char *buffer;
+	struct sockaddr_in serverAddress, clientAddress;
+
+	if (argc < 1) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+
+	// Set up the address struct for this process (the server)
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
+
+	// Set up the socket
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (listenSocketFD < 0) error("ERROR opening socket");
+
+	// Enable the socket to begin listening
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+		error("ERROR on binding");
+	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+	pid_t mypid = -5;
+	pid_t myotherpid = -5;
+	int pstat = -5;
+	while(1){
+		// Accept a connection, blocking if one is not available until one connects
+		do{
+		}while(numchild>=5);
+		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		if (establishedConnectionFD < 0) error("ERROR on accept");
+		charsRead = send(establishedConnectionFD, "dec", 4, 0);
+		if (charsRead < 0) error("ERROR writing to socket");
+		mypid = fork();
+		if(mypid<0){
+			error("Error creating process");
+		}
+		else if(mypid == 0){
+			//get size of key
+			buffer = calloc(256,256);
+			charsRead = recv(establishedConnectionFD, buffer, 24, 0);
+			int len;
+			len = atoi(buffer);
+			//send success for key size
+			charsRead = send(establishedConnectionFD, "success", 16, 0);
+			//allocate buffer for key and get it
+			free(buffer);
+			buffer = calloc(len,(len+1)*sizeof(char));
+			charsRead = recv(establishedConnectionFD, buffer, len, 0);
+			if(charsRead<len) error("Error receiving data\n");
+			//copy key from buffer into mykey
+			char *mykey;
+			mykey = malloc((len+1)*sizeof(char));
+			strcpy(mykey,buffer);
+			//printf("key %p: %s\n",mykey,mykey);
+			//send success for key
+			charsRead = send(establishedConnectionFD, "success", 16, 0);
+
+			//get size of text
+			int len2;
+			free(buffer);
+			buffer = calloc(256, 256);
+			charsRead = recv(establishedConnectionFD, buffer, 24, 0);
+			if(charsRead<0) error("Error receiving data\n");
+			len2 = atoi(buffer);
+			free(buffer);
+			buffer = calloc(len2,(len2+1)*sizeof(char));
+			//send success for size
+			charsRead = send(establishedConnectionFD, "success", 16, 0);
+			//read text
+			charsRead = recv(establishedConnectionFD, buffer, len2, 0);
+			if(charsRead<len2) error("Error receiving data\n");
+			//copy text into plaintext
+			char *plaintext;
+			plaintext = malloc((len2+1)*sizeof(char));
+			strcpy(plaintext,buffer);
+			//printf("SERVER - cyphertext: %s\n",plaintext);
+			int i,c;
+			int temp,temp2;
+			char *cyphertext;
+			cyphertext = malloc((len2+1)*sizeof(char));
+
+			//encryption
+
+			int tempc;
+			c=0;
+			for(i=0;i<len2;i++){
+				temp = 0;
+				temp2 = 0;
+				tempc = 0;
+				if(plaintext[i]=='\n'){
+					break;
+				}
+				else{
+					temp = (opsout(plaintext[i]) - opsout(mykey[c]));
+                    if(temp<0){
+                        temp+=27;
+                    }
+					cyphertext[i] = toOps(temp);
+					if(c==len){
+						c=0;
+					}
+				}
+				c++;
+			}
+			cyphertext[len2] = '\0';
+			//free and exit child
+			//printf("SERVER - plaintext: %s\n",cyphertext);
+			charsRead = send(establishedConnectionFD, cyphertext, len2, 0);
+			close(establishedConnectionFD); // Close the existing socket which is connected to the client
+			free(mykey);
+			free(buffer);
+			free(plaintext);
+			exit(0);
+		}
+		else{
+			//increment number of children processes and reap children
+			numchild++;
+			close(establishedConnectionFD);
+			signal(SIGCHLD, SIG_IGN);
+			myotherpid = waitpid(mypid, &pstat, WNOHANG);
+		}
+	}
+	close(listenSocketFD); // Close the listening socket
+	return 0;
+}
